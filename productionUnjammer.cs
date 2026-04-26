f75b3dae4dc963e4a2f95bfc36b18093cc5d624e
AlexeyPG <AlexeyPG.rus@gmail.com>
2026-04-26 18:01:29 +0300

    small changes

--- productionUnjammer.cs
+++ productionUnjammer.cs
@@ -1,29 +1,33 @@
 // Production unjammer script by AlexeyPG
 // Version 1
+// Prevents assemblers from stopping when their inventory is overfilled by clearing them
 // barely tested, hopefully optimized
 
-public int scanInterval = 10;
-//After how many 100 ticks programs scans whole grid to find production blocks and inventories
-//1 - every tick
-//2 - every 2nd tick
-//5 - every 5th tick
+public int scanInterval = 0;
+// After how many 100 ticks programs scans whole grid to find production blocks and inventories
+// WARNING! Grids with large amount of functional blocks may heavily affect game simulation speed, scans may cause significant lag spikes
+// 10 - every 10th tick (16.6 sec)
+// 0 - scan only once when script recompiled or world started
 
 public int clearInterval = 3;
-//After how many 100 ticks program checks every scanned assembler for being overfilled
-//1 - every tick
-//2 - every 2nd tick
-//5 - every 5th tick
+// After how many 100 ticks program checks every scanned assembler for being overfilled
+// 1 - every tick (1.6 sec)
+// 2 - every 2nd tick
+// 5 - every 5th tick
 
 public float clearRatio = 0.75f;
-//Ratio over which production block will be emptied
-//1f - only 100% full inventory (not recommended)
-//0.75f - if 3000L (75%) of 4000L inventory is filled it will be cleared
+// Ratio over which production block will be emptied
+// 1f - only 100% full inventory (not recommended)
+// 0.75f - if 3000L (75%) of 4000L inventory is filled it will be cleared
 
 public bool onlyProgramGrid = true;
-//Scan inventories only on a grid where program block is placed
-//true - only a grid with program block will be scanned and inventories from other grids won't be used
-//false - inventory from any connected grid can send items to any other connected grid
+// Scan inventories only on a grid where program block is placed
+// true - only a grid with program block will be scanned and inventories from other grids won't be used
+// false - inventory from any connected grid can send items to any other connected grid
 
+public bool ignoreRefineries = true;
+// Refineries are also production blocks
+// They usually clear on their own and will refine anything they can accept, so there is no need to think about them
 
 
 
@@ -43,20 +47,27 @@ public void Main(string argument, UpdateType updateSource)
     Echo("Operational for " + tick + " ticks");
     if(!enoughSpace) Echo("Not enough space");
 
-    if(tick % scanInterval == 0){
+    if(scanInterval != 0) if(tick % scanInterval == 0){
         scan();
     }
-    if(tick % clearInterval == 0){
+    if(clearInterval != 0) if(tick % clearInterval == 0){
         Echo("Clearing");
+        bool foundAny = false;
         foreach(IMyTerminalBlock assembler in assemblers){
             for(int i = 0; i < assembler.InventoryCount; i++){
                 IMyInventory inv = assembler.GetInventory(i);
-                if(inv.VolumeFillFactor >= clearRatio) clear(inv);
+                if(inv.VolumeFillFactor >= clearRatio){
+                    Echo("Found overfilled production block " + assembler.CustomName);
+                    foundAny = true;
+                    clear(inv);
+                }
             }
         }
+        if(!foundAny){
+            enoughSpace = true;
+            Echo("Everything good!");
+        } 
     }
-
-    
     tick++;
 }
 
@@ -64,7 +75,7 @@ public void clear(IMyInventory inv){
         List<MyInventoryItem> itemsInInv = new List<MyInventoryItem>{};
         inv.GetItems(itemsInInv);
             foreach(MyInventoryItem invItem in itemsInInv){
-                Echo("Searching for inventories with similar item");
+                //Echo("Searching for inventories with similar item");
                 List<IMyInventory> sameItemInvs = new List<IMyInventory>{};
                 foreach(IMyInventory inv_ in invs){
                     if(inv_.FindItem(invItem.Type)!=null){
@@ -88,7 +99,7 @@ public void clear(IMyInventory inv){
                     }
                 }
                 if(inv.GetItemAmount(invItem.Type).RawValue > 0){
-                    Echo("Searching for filled inventories");
+                    //Echo("Searching for filled inventories");
                     List<IMyInventory> fittingInvs = new List<IMyInventory>{};
                     foreach(IMyInventory inv_ in invs){
                         if(inv_.CanItemsBeAdded(invItem.Amount, invItem.Type)){
@@ -113,7 +124,7 @@ public void clear(IMyInventory inv){
                     }
                 }
                 if(inv.GetItemAmount(invItem.Type).RawValue > 0){
-                    Echo("Searching for largest inventories");
+                    //Echo("Searching for largest inventories");
                     List<IMyInventory> largestInvs = new List<IMyInventory>{};
                     foreach(IMyInventory inv_ in invs){
                         if(inv_.CanTransferItemTo(inv_, invItem.Type)){
@@ -157,7 +168,9 @@ public void scan(){
             if(block != null)
             if(block.CubeGrid == Me.CubeGrid || !onlyProgramGrid)
             if(block is IMyProductionBlock){
-                assemblers.Add(block);
+                if(block is IMyRefinery){
+                    if(!ignoreRefineries) assemblers.Add(block);
+                } else assemblers.Add(block);
             }
         }
         foreach(IMyTerminalBlock block in blocks){
